afb86745ee3ecc004794bd54f33fef1ad7ce7a90
Vadim Zaretskiy <vadizar@mobidevices.ru>
2017-07-03 21:48:45 +0300

    Fix default color in SVG

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,14 @@
 var
     gulp = require('gulp'),
-    watch = require('gulp-watch'),
-    sequence = require('run-sequence'),
-    stylus = require('gulp-stylus'),
-    csso = require('gulp-csso'),
-    rename = require("gulp-rename"),
-    del = require("del"),
-    nib = require('nib');
+    $ = require('gulp-load-plugins')({
+        pattern: '*'
+    });
 
 // Compiling Stylus in CSS
 gulp.task('css', function() {
     gulp.src('./*.styl')
-        .pipe(stylus({
-            use: nib()
+        .pipe($.stylus({
+            use: $.nib()
         }))
         .pipe(gulp.dest('./'))
 });
@@ -20,29 +16,29 @@ gulp.task('css', function() {
 // Compiling Stylus in CSS min
 gulp.task('cssmin', function() {
     gulp.src('./*.styl')
-        .pipe(stylus({
-            use: nib()
+        .pipe($.stylus({
+            use: $.nib()
         }))
-        .pipe(csso())
-        .pipe(rename('godlike.min.css'))
+        .pipe($.csso())
+        .pipe($.rename('godlike.min.css'))
         .pipe(gulp.dest('./'))
 });
 
 // Watcher
 gulp.task('watch', function() {
-    watch('./*.styl', function() {
+    $.watch('./*.styl', function() {
         gulp.start('css')
     });
 });
 
 // Clean old CSS
 gulp.task('clean', function() {
-    del('*.css')
+    $.del('*.css')
 });
 
 // Main task
 gulp.task('default', function(cb) {
-    return sequence(
+    return $.sequence(
         'clean','watch','css','cssmin',
         cb);
 });
